ff998a97a2e1fc52da15113b6f930408940be9ba
Alexandr_BE <yaa2215@yandex.ru>
2022-10-07 23:51:28 +0300

    add Task_21

---
+++ Homework_lesson_03/Task_19/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+*/
+
+void Is_Palindrom(int[] processablenumber)
+{   
+    int result, index;
+    int[] array = new int[5];
+    // Console.WriteLine("Введите пятизначное число:");
+    // processablenumber = Convert.ToInt32(Console.ReadLine());
+
+    index = 0;
+    while (processablenumber > 0)
+    {
+        result = processablenumber % 10;
+        processablenumber = processablenumber / 10;
+        array[index] = result;
+        Console.WriteLine(array[index]);
+        index++;
+        
+    }
+    
+    // if (result >= 0)
+    // {
+    //     Console.WriteLine($"{number} -> {result}");
+    // }
+    // else Console.WriteLine($"{number} -> третьей цифры нет");
+}
+
+    Console.WriteLine("Введите пятизначное число:");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Is_Palindrom(number);
---
+++ Homework_lesson_03/Task_21/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Напишите программу, которая принимает на вход координаты двух точек 
+и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+double Distance_Between_Points()
+{
+    Console.WriteLine("Введите координату Х точки 1:");
+    int point_1_X = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите координату Y точки 1:");
+    int point_1_Y = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите координату Z точки 1:");
+    int point_1_Z = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Х точки 2:");
+    int point_2_X = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите координату Y точки 2:");
+    int point_2_Y = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите координату Z точки 2:");
+    int point_2_Z = Convert.ToInt32(Console.ReadLine());
+
+    double result = Math.Sqrt(Math.Pow((point_2_X - point_1_X), 2)
+                            + Math.Pow((point_2_Y - point_1_Y), 2)
+                            + Math.Pow((point_2_Z - point_1_Z), 2)
+                            );
+    return result;
+}
+
+double result = Distance_Between_Points();
+Console.WriteLine(Math.Round(result, 2));
