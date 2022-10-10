5337b58f658cd99d006bacb225c773a7d9269429
Alexandr_BE <yaa2215@yandex.ru>
2022-10-10 20:16:59 +0300

    add Homework_lesson_04

---
+++ Homework_lesson_04/Task_27/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+
+int SumOfFigures(int number)
+{
+    int sum = 0;
+    int processablenumber = Math.Abs(number);
+    int index = 0;
+    while (processablenumber > 0)
+    {
+        sum = sum + processablenumber % 10;
+        processablenumber = processablenumber / 10;
+        index++;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = SumOfFigures(num);
+Console.WriteLine($"{num} -> {res}");
---
+++ Homework_lesson_04/Task_29/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 29: Напишите программу, в которой пользователь задает длину массива, 
+элементы которого задаются в диапазоне [1,99] и выводит на экран .
+
+5 -> [1, 2, 5, 7, 19]
+
+3 -> [6, 1, 33]
+*/
+
+void FillArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]= new Random().Next(1, 100);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i==array.Length-1) Console.Write($"{array[i]}");
+        else Console.Write($"{array[i]}, ");
+    }
+    Console.Write("]");
+}
+
+Console.WriteLine("Введите длину массива:");
+int lenghtArray = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[lenghtArray];
+FillArray(array);
+Console.Write($"{lenghtArray} -> ");
+PrintArray(array);
+Console.WriteLine();
