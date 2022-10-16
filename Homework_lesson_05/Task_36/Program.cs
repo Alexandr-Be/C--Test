15d9bdcf49402734fe1a260a810238bbfcbf5887
Alexandr_BE <yaa2215@yandex.ru>
2022-10-16 12:29:07 +0300

    add Task 36

---
+++ Homework_lesson_05/Task_36/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0
+*/
+
+void FillArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-99, 100);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1) Console.Write($"{array[i]}");
+        else Console.Write($"{array[i]}, ");
+    }
+    Console.Write("]");
+}
+
+int CountEvenPositions(int[] array)
+{
+    int count = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        count += array[i];
+
+    }
+    return count;
+}
+
+Console.WriteLine("Введите длину массива:");
+int lenghtArray = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[lenghtArray];
+FillArray(array);
+PrintArray(array);
+int countedNumbers = CountEvenPositions(array);
+Console.Write($" -> {countedNumbers}");
+Console.WriteLine();
---
+++ Homework_lesson_05/Task_37/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+Задача 37(сложная необязательная): Найдите произведение пар чисел в одномерном массиве.
+Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+Результат запишите в новом массиве.
+[1 2 3 4 5] -> 5 8 3
+[6 7 3 6] -> 36 21
+*/
+
+void FillArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1) Console.Write($"{array[i]}");
+        else Console.Write($"{array[i]}, ");
+    }
+    Console.Write("]");
+}
+
+int CountEvenPositions(int[] array)
+{
+    int count = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        count += array[i];
+
+    }
+    return count;
+}
+
+Console.WriteLine("Введите длину массива:");
+int lenghtArray = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[lenghtArray];
+FillArray(array);
+PrintArray(array);
+int countedNumbers = CountEvenPositions(array);
+Console.Write($" -> {countedNumbers}");
+Console.WriteLine();
