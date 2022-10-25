9be36ceb41432cf32586f58f406b7aa424c2684c
Alexandr_BE <yaa2215@yandex.ru>
2022-10-25 18:27:33 +0300

    add Final Project Task 64

---
+++ Homework_final_project/Task_64/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+void ShowNumbers(int n)
+{
+    if (n==0) return;
+    Console.Write($"{n}, ");
+    ShowNumbers (n - 1);
+}
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write( $"N = {number} -> \"");
+ShowNumbers(number);
+Console.SetCursorPosition(Console.CursorLeft - 2, Console.CursorTop);
+Console.Write("\x1B[1P");
+Console.Write( $"\"");
+Console.WriteLine();
---
+++ Homework_lesson_08/Task_60/Program.cs
@@ -0,0 +1,82 @@
+﻿/*
+Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)
+*/
+
+void FillArray(int[,,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int k = 0; k < matr.GetLength(2); k++)
+            {
+                // matr[i,j,k] = new Random().Next(1,100);
+                matr[i, j, k] = 5;
+            }
+        }
+    }
+}
+
+int[,,] ChekMatrix(int[,,] matr)
+{
+    int[,,] newMatr = new int[matr.GetLength(0), matr.GetLength(1), matr.GetLength(2)];
+    int CompareElement(int l, int m, int n)
+    {
+        for (int l = 0; l < matr.GetLength(0); l++)
+        {
+            for (int m = 0; m < matr.GetLength(1); m++)
+            {
+                for (int n = 0; n < matr.GetLength(2); n++)
+                {
+                    if (matr[l,m,n]!=CompareElement(l+1,m,n)) return matr[l,m,n];
+                    else
+                        {
+                           matr[l,m,n] = new Random().Next(1,100);
+                           CompareElement(l,m,n);
+                        }
+                }
+            }
+        }
+    }
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int k = 0; k < matr.GetLength(2); k++)
+            {
+                newMatr[i, j, k] = CompareElement(i, j, k);
+            }
+        }
+    }
+    return newMatr;
+}
+
+
+void PrintMatr(int[,,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int k = 0; k < matr.GetLength(2); k++)
+            {
+                Console.Write($"{matr[i, j, k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+
+int[,,] matrix = new int[2, 2, 2];
+FillArray(matrix);
+int[,,] chekedMatrix = ChekMatrix(matrix);
+
+PrintMatr(chekedMatrix);
+Console.WriteLine();
